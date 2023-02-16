06302b32da99fce8b07e00205ca56da94dc668d3
a <a@b.com>
2023-02-16 20:03:23 +0900

    1st commmit

--- src/view.py
+++ src/view.py
@@ -1,24 +1,20 @@
-#文字化けを防ぐため
 # coding: utf-8
 
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-# appという名前でFlaskオブジェクト をインスタンス化
+# app という変数でFlaskオブジェクトをインスタンス化
 app = Flask(__name__)
 
-# --- View側の設定 ---
-# ルートディレクトリにアクセスした場合の挙動
+# --- View 側の設定 ---
+# rootディレクトリにアクセスした場合の挙動
 @app.route('/')
-# def以下がアクセス後の操作
-def index():
-    # DBから以下の変数を読み込んできたと仮定
-    title_ = 'ようこそ'
-    message = 'MTVデザインパターンでWebアプリ作成'
-    
-    # return 'Hello World!'
-    return render_template('index.html',title=title_, massage=message)
 
-# エントリーポイント
-if __name__=='__main__':
-   app.run()
+# def 以下がアクセス後の操作
+def index():
+    # return 'Hello World!'
+    return render_template('index.html') #追加
+
+# メイン関数
+if __name__ == '__main__':
+    app.run()
 
